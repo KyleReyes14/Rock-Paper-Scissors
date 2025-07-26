c1a3912ef4654bf52773eae032d557a26f862d73
KyleReyes14 <kylejacobreyes901@gmail.com>
2025-07-26 22:00:11 +0800

    feat(player-inputs): Added player turn container)

--- scripts.js
+++ scripts.js
@@ -11,3 +11,25 @@ function getComputerChoice () {
         return computerPick
     }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const playerRock = document.getElementById("player-rock");
+    const playerPaper = document.getElementById("player-paper");
+    const playerScissors = document.getElementById("player-scissors");
+    const playerPickContainer = document.getElementById("player-pick");
+    
+    playerRock.addEventListener("click", () => {
+        let playerPickContainer = document.getElementById("player-pick")
+        playerPickContainer.innerHTML = `<img src="./images/rock-inserted.png" alt="Rock hand" class="inserted-img">`
+    })  
+
+    playerScissors.addEventListener("click", () => {
+        let playerPickContainer = document.getElementById("player-pick")
+        playerPickContainer.innerHTML = `<img src="./images/scissors-inserted.png" alt="Scissors hand" class="inserted-img">`
+
+        playerPaper.addEventListener("click", () => {
+        let playerPickContainer = document.getElementById("player-pick")
+        playerPickContainer.innerHTML = `<img src="./images/paper-inserted.png" alt="Paper hand" class="inserted-img">`
+    })  
+    })  
+})
