3a809ab6f1caf95f268909ac344a025d00ad64e9
KyleReyes14 <kylejacobreyes901@gmail.com>
2025-07-26 22:30:04 +0800

    feat(comp): added event listener for computer choice

--- scripts.js
+++ scripts.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const playerRock = document.getElementById("player-rock");
     const playerPaper = document.getElementById("player-paper");
     const playerScissors = document.getElementById("player-scissors");
-    const playerPickContainer = document.getElementById("player-pick");
+    const computerPickContainer = document.getElementById("computer-pick");
     
     playerRock.addEventListener("click", () => {
         let playerPickContainer = document.getElementById("player-pick")
@@ -26,10 +26,27 @@ document.addEventListener("DOMContentLoaded", () => {
     playerScissors.addEventListener("click", () => {
         let playerPickContainer = document.getElementById("player-pick")
         playerPickContainer.innerHTML = `<img src="./images/scissors-inserted.png" alt="Scissors hand" class="inserted-img">`
+    })
 
         playerPaper.addEventListener("click", () => {
         let playerPickContainer = document.getElementById("player-pick")
         playerPickContainer.innerHTML = `<img src="./images/paper-inserted.png" alt="Paper hand" class="inserted-img">`
-    })  
-    })  
+    })
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    const submit = document.getElementById("submit-btn")
+
+    submit.addEventListener("click", () => {
+        const computerPickContainer = document.getElementById("computer-pick")
+        
+        getComputerChoice()
+        if (computerPick === "Rock") {
+            computerPickContainer.innerHTML = `<img src="./images/rock-inserted.png" alt="Rock hand" class="inserted-img">`
+        } else if (computerPick === "Scissors") {
+            computerPickContainer.innerHTML = `<img src="./images/scissors-inserted.png" alt="Scissors hand" class="inserted-img">`
+        } else {
+            computerPickContainer.innerHTML = `<img src="./images/paper-inserted.png" alt="Paper hand" class="inserted-img">`
+        }
+    })
 })
